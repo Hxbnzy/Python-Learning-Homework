decec17061acf24dbb2d5b923a044ae48fdaf56f
Qi Liu <Hxbnzy@gmail.com>
2018-03-31 16:33:13 +0800

    🚲🚲🚲

--- 1.温度转换/TempConvert.py
+++ 1.温度转换/TempConvert.py
@@ -7,10 +7,10 @@ import re
 TempStr = input("请输入带有符号的温度值:")
 # re.match(r"F|f$", TempStr) 不匹配
 # 🤣 没弄清楚为啥 等以后有时间
-if re.match(r"^F|f", TempStr) or re.search(r"F|f$", TempStr):
+if re.match(r"(^F|f)\d+$", TempStr) or re.search(r"^\d+(F|f$)", TempStr):
     C = (eval(re.sub(r'\D', "", TempStr)) - 32) / 1.8
     print("转换后的温度是{:.2f}C".format(C))
-elif re.match(r"^C|c", TempStr) or re.search(r"C|c$", TempStr):
+elif re.match(r"(^C|c)\d+$", TempStr) or re.search(r"^\d+(C|c$)", TempStr):
     F = 1.8 * eval(re.sub(r'\D', "", TempStr)) + 32
     print("转换后的温度是{:.2f}F".format(F))
 else:
