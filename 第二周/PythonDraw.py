2f2fe2726b41bc9bb727511388b0b8a9b14f1456
Qi Liu <Hxbnzy@gmail.com>
2018-04-02 18:48:32 +0800

    🎉

--- 第二周/PythonDraw.py
+++ 第二周/PythonDraw.py
@@ -1,46 +1,51 @@
 import turtle
-
-turtle.setup(800, 800, 200, 200)
-turtle.pensize(2)
-for i in range(1):
-    turtle.circle(80, 360)
-    turtle.circle(-80, 360)
-    turtle.left(90)
-    turtle.circle(80, 360)
-    turtle.circle(-80, 360)
-    turtle.penup()
-    turtle.right(90)
-    turtle.fd(-160)
-    turtle.pendown()
-    turtle.left(90)
-    turtle.circle(-165, 360)
-
-# turtle.circle(80, 360)
-# turtle.circle(-80, 360)
-# turtle.circle(100, 360)
-# turtle.circle(-100, 360)
-# turtle.circle(120, 360)
-# turtle.circle(-120, 360)
+from time import sleep
 #
-# turtle.left(90)
-# turtle.circle(80, 360)
-# turtle.circle(-80, 360)
-# turtle.circle(100, 360)
-# turtle.circle(-100, 360)
-# turtle.circle(120, 360)
-# turtle.circle(-120, 360)
-# turtle.penup()
-# turtle.fd(-250)
-# turtle.pendown()
-# turtle.pensize(25)
-# turtle.pencolor("purple")
-# turtle.seth(-40)
-# for i in range(4):
-#     turtle.circle(40, 80)
-#     turtle.circle(-40, 80)
-# turtle.circle(40, 80/2)
-# turtle.fd(40)
-# turtle.circle(25, 180)
-# turtle.fd(40 * 2/3)
-turtle.done()
+# turtle.setup(800, 800, 200, 200)
+# turtle.pensize(2)
+# for i in range(1):
+#     turtle.circle(80, 360)
+#     turtle.circle(-80, 360)
+#     turtle.left(90)
+#     turtle.circle(80, 360)
+#     turtle.circle(-80, 360)
+#     turtle.penup()
+#     turtle.right(90)
+#     turtle.fd(-160)
+#     turtle.pendown()
+#     turtle.left(90)
+#     turtle.circle(-165, 360)
+#
+# turtle.done()
 
+# heart
+# import turtle as t
+# import math as m
+#
+# t.setup()
+#
+# t.fillcolor("red")
+# t.seth(130)
+# t.begin_fill()
+# t.circle(100, 200)
+# a = 100*m.cos(m.radians(40))+100*m.sin(m.radians(30))
+# aa = a/m.cos(m.radians(30))
+# t.fd(aa)
+# t.up()
+# t.goto(0, 0)
+# t.down()
+# t.seth(50)
+# t.circle(-100, 200)
+# t.fd(aa)
+# t.end_fill()
+# t.up()
+# t.goto(300, 300)
+#
+# t.done()
+
+from pylab import*
+t = linspace(0, 2*pi, 100)
+x = sin(2*t) + 2*sin(t)
+y = - cos(2*t)-2*cos(t)
+fill(x, y, 'r')
+show()
